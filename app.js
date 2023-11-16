e71aef9c34555f46dd2704e9eacae8f002e7314d
brrrun <brunomsantosmendes@gmail.com>
2023-11-16 10:50:42 +0000

    2nd try regarding cors

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = 5005;
 
-const domainList = ['http://localhost:5173', 'https://effortless-salmiakki-807f97.netlify.app/']
-
+const domainList = [
+  "http://localhost:5173",
+  "https://effortless-salmiakki-807f97.netlify.app/",
+];
 
 server.use(middlewares);
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', '*')
+  let origin = req.headers.origin;
+  if (domainList.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 server.use(router);
