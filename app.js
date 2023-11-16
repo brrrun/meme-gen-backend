3aca2e2fc45291b4ed01be664a8051c8c82e43fb
brrrun <brunomsantosmendes@gmail.com>
2023-11-16 12:45:18 +0000

    2nd try regarding cors

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const morgan = require("morgan");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults();
 const PORT = 5005;
 
 server.use(middlewares);
 server.use(morgan("dev"));
-
+server.use((req, res, next) => {
+  // Middleware to disable CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 server.use(router);
 
 server.listen(PORT, () => {
