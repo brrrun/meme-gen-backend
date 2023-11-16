5639ff358fea759870cad7ad0798fd7e3ba8b0b0
brrrun <brunomsantosmendes@gmail.com>
2023-11-16 10:38:02 +0000

    with cors configs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const jsonServer = require("json-server");
 const morgan = require("morgan");
+const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = 5005;
+
+const domainList = ['http://localhost:5173', 'https://effortless-salmiakki-807f97.netlify.app/']
+
 
 server.use(middlewares);
 server.use(morgan("dev"));
@@ -15,6 +19,7 @@ server.use((req, res, next) => {
   next();
 });
 server.use(router);
+server.use(cors({origin: domainList}))
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
