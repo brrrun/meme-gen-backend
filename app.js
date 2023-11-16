0faaf564710f2e6a4b9eefe3f1a9d6ad07a5a281
brrrun <brunomsantosmendes@gmail.com>
2023-11-16 11:29:42 +0000

    2nd try regarding cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const jsonServer = require("json-server");
 const morgan = require("morgan");
+const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -9,8 +10,16 @@ const PORT = 5005;
 
 server.use(middlewares);
 server.use(morgan("dev"));
+server.use(
+  cors({
+      origin: true,
+      credentials: false,
+      preflightContinue: false,
+  })
+);
+
+server.options('*', cors());
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
   });
