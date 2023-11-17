af57c7e69f48dfb2956b649de74f219aee44df8a
brrrun <brunomsantosmendes@gmail.com>
2023-11-17 09:05:32 +0000

    backend update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const PORT = "https://memezard-backend.onrender.com";
+const PORT = 5005;
 
 server.use(middlewares);
 server.use(morgan("dev"));
